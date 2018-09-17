7d0a575259ad697b36bca21baef35eb307bec717
Arthur Ezenwanne <arthurezenwanne@outlook.com>
2018-09-17 21:52:20 +0100

    Update practice file code

--- practice.py
+++ practice.py
@@ -1,1 +1,10 @@
 #!/usr/bin/env python3
+
+def main():
+    game = ['Ludo', 'Snakes and Ladders', 'Card', 'I Call On']
+    print_list(game)
+
+def print_list(x_game):
+    for i in x_game: print(i, end = ' ', flush=True)
+
+if __name__ == '__main__': main()
