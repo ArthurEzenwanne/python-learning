7563fd8324c67fc2b107e1ef3961360df9bf8877
Arthur Ezenwanne <arthurezenwanne@outlook.com>
2018-09-22 20:14:11 +0100

    Edit pratice code

--- practice.py
+++ practice.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 
 def main():
-    game = ['Ludo', 'Snakes and Ladders', 'Card', 'I Call On', 'Monopoly']
+    #game = ['Ludo', 'Snakes and Ladders', 'Card', 'I Call On', 'Monopoly']
     animal_sounds = {'kitten':'meows', 'puppy':'ruff!', 'lion':'roars'}
-    print_list(game)
-    print_animal_sounds(animal_sounds)
+    #print_list(game)
+    #print_animal_sounds(animal_sounds)
+    for v in animal_sounds.values(): print(v)
+    for k in animal_sounds.keys(): print(k)
     
 def print_list(x_game):
     for i in x_game: print(i, end = ' ', flush=True)
